6d76a7570436007ad9c7e9fd80ee89f8ca9d9c2b
dineo789 <dineo.thato06@gmail.com>
2018-12-11 23:35:37 +0200

    Code clean up

--- app/src/main/java/com/architecture/android/navigationcomponent/ContentFragment.kt
+++ app/src/main/java/com/architecture/android/navigationcomponent/ContentFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class WelcomeFragment : Fragment() {
+
+class ContentFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        return inflater.inflate(R.layout.fragment_content, container, false)
     }
+
+
 }
