5e6d03d369be7d2db26f682c4fad0060df70997a
dineo789 <dineo.thato06@gmail.com>
2018-12-11 23:50:23 +0200

    Navigate from one screen to next

--- app/src/main/java/com/architecture/android/navigationcomponent/WelcomeFragment.kt
+++ app/src/main/java/com/architecture/android/navigationcomponent/WelcomeFragment.kt
@@ -5,13 +5,21 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var button: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        //Navigation by action
+        view?.findViewById<Button>(R.id.nextButton)
+            ?.setOnClickListener { Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_contentFragment) }
         return inflater.inflate(R.layout.fragment_welcome, container, false)
+
+
     }
 }
